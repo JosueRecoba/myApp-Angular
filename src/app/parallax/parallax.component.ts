ff8a4dbf7f0abe4a8ca75b2bd217a1e88f931606
Josue Recoba Huerta <josue.huer.reco.22@gmail.com>
2024-03-13 10:02:22 -0600

    Agregado componente Parallax

---
+++ src/app/parallax/parallax.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parallax',
+  standalone: true,
+  imports: [],
+  templateUrl: './parallax.component.html',
+  styleUrl: './parallax.component.css'
+})
+export class ParallaxComponent {
+}
